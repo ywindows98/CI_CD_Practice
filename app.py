cb8dc3e401052e67756e1896d1953af9e98b837c
ywindows98 <tkachenko.iwn@gmail.com>
2025-03-31 12:12:48 +0200

    Update

--- app.py
+++ app.py
@@ -25,10 +25,15 @@ def connect():
 	if "POSTGRES_HOST" in os.environ:
 		host = os.environ["POSTGRES_HOST"]
 		connection = psycopg2.connect(host)
+		cur = connection.cursor()
+		cur.execute("SELECT * FROM vendors;")
+		rows = cur.fetchall()
+		cur.close()
 		connection.close()
-		return f"<h1 style='color:blue'>Connected!</h1>"
+		return f"<h1 style='color:blue'>{rows}!</h1>"
 	else:
 		return "<h1 style='color:red'>No host provided!</h1>"
+
 
 
 if __name__ == "__main__":
