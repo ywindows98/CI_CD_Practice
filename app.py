d2892542e636c547d9e9637f8370b7bbb7de60c4
ywindows98 <tkachenko.iwn@gmail.com>
2025-04-07 11:02:03 +0200

    update

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	name = "Ferko2"
+	name = "Ferko3"
 	if "MY_NAME" in os.environ:
 		name = os.environ["MY_NAME"]
 	return "<h1 style='color:blue'>Hello There {}!</h1>".format(name)
